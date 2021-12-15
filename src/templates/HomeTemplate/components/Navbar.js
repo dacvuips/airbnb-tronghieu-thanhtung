4e8992f79777f315d0715c656959607230ac9a41
tunght994 <tunght994@gmail.com>
2021-12-15 16:01:05 +0700

    thanhtung

--- src/templates/HomeTemplate/components/Navbar.js
+++ src/templates/HomeTemplate/components/Navbar.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import logo from './../../../assets/logo.png'
 
 const Navbar = () => {
     return (
-        <div>
+        <div className='navbar'>
             Navbar
         </div>
     )
